94dc77ebf189690f71fa7be4e67ad03bf6a3fb35
panjd123 <1747366367@qq.com>
2024-09-25 04:12:14 +0800

    Use new autograder

--- test/gemm_test2.py
+++ test/gemm_test2.py
@@ -7,10 +7,25 @@ def test_csim2():
     # miss_cache, miss_reg, latency, baseline
 
     latency_case1, baseline_case1 = result[2], result[3]
-    assert latency_case1 < baseline_case1, "You should optimize case1"
+    # assert latency_case1 < baseline_case1, "You should optimize case1"
 
     latency_case2, baseline_case2 = result[6], result[7]
-    assert latency_case2 < baseline_case2, "You should optimize case2"
+    # assert latency_case2 < baseline_case2, "You should optimize case2"
 
     latency_case3, baseline_case3 = result[10], result[11]
-    assert latency_case3 < baseline_case3, "You should optimize case3"
+    # assert latency_case3 < baseline_case3, "You should optimize case3"
+
+    score = 0
+    weighted_speedup = result[-1]
+
+    if weighted_speedup <= 1:
+        score = 0
+    else:
+        score = (weighted_speedup - 1) * 100
+
+    with open(".autograder_result", "w") as f:
+        f.write(str(score))
+
+
+if __name__ == "__main__":
+    test_csim2()
